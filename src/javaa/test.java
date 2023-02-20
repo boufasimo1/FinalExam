150ed760e2d8645db823eeb1cd1d3aca0701e837
boufasimo1 <120329116+boufasimo1@users.noreply.github.com>
2023-02-20 01:59:00 -0600

    FirstCommit

---
+++ src/javaa/test.java
@@ -0,0 +1,14 @@
+package javaa;
+
+
+
+public class test {
+public static void main(String[] args) {
+	
+String g = new String("hello");
+String j = "hello";
+System.out.println(g);
+System.out.println(j);
+	
+}
+}
